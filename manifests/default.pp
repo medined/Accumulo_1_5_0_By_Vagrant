fa2ebc047e7604935d551fefd7c1ba47f688233b
David Medinets <david.medinets@gmail.com>
2014-01-13 21:50:00 -0500

    Use puppet to update hosts file instead of vagrant hostmanager plugin.

---
+++ manifests/default.pp
@@ -0,0 +1,13 @@
+
+
+host { 'affy-master.example.com':
+    ip => '10.211.55.100'
+}
+
+host { 'affy-slave1.example.com':
+    ip => '10.211.55.101'
+}
+
+host { 'affy-slave2.example.com':
+    ip => '10.211.55.102'
+}
